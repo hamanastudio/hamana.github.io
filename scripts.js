ac592dbb2c2d97b1611348573aca9072d9e80e16
hamana <hamanastudios@gmail.com>
2025-04-21 23:45:23 +0900

    Delete scripts.js

--- scripts.js
+++
@@ -1,6 +0,0 @@
-// ハンバーガーメニューの表示/非表示を切り替える
-document.getElementById('hamburger-menu').addEventListener('click', function() {
-    const dropdown = document.getElementById('hamburger-dropdown');
-    dropdown.style.display = (dropdown.style.display === 'block') ? 'none' : 'block';
-});
-
